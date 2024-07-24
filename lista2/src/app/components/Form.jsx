06f5997b6ac82f4dabaed6ef4c78290610e06058
Karla Murillo <74341989+karlamurillo08@users.noreply.github.com>
2024-07-23 21:44:18 -0600

    Add files via upload

---
+++ lista2/src/app/components/Form.jsx
@@ -0,0 +1,45 @@
+"use client"
+import React, {useState} from 'react'
+import Todo from './Todo'
+import styles from "../page.module.css"
+
+const Form = () => {
+    const [todo,setTodo]=useState({})
+    const [todos,setTodos]=useState([
+        {todo:'todo 1'},
+        {todo:'todo 2'},
+        {todo:'todo 3'}
+    ])
+    const handleChange=e=>setTodo({[e.target.name]:e.target.value})
+    const handleClick=e=>{
+        if(Object.keys(todo).length===0|| todo.todo.trim()===''){
+            alert('el campo no puede ser vacio')
+            return
+        }
+        setTodos([...todos,todos])
+    }
+
+    const deleteTodo=indice=>{
+        const newTodos=[...todos]
+        newTodos.splice(indice,1)
+        setTodos(newTodos)
+    }
+
+
+return (
+<>
+<form onSubmit={e=>e.preventDefault()}>
+    <label>Agregar tarea</label><br/>
+    <input className={styles.form_input} type="text" name='todo' onChange={handleChange} />
+    <button className={styles.form_button} onClick={handleClick}>Agregar</button>
+
+
+</form>
+{
+    todos.map((value,index)=>(
+    <Todo todo={value.todo}/>))
+}
+</>
+)
+}
+export default Form
